21f5c522d00934e664769704f0c8e4c01295988d
Shivam Bansal <shivambnsal@gmail.com>
2024-08-22 22:02:58 +0530

    Change reading status color dynamically

--- script.js
+++ script.js
@@ -23,17 +23,18 @@ addToLibrary(Book1);
 let Book2 = new Book("Book2", "Author2", 35, "Read");
 addToLibrary(Book2);
 
-
 let cardContainer = document.querySelector('.card-container') ;
 
 myLibrary.forEach((book) => {
+    const statusCssClass = book.status == 'Read' ? "read" : "not-read";
     cardContainer.innerHTML += `
         <div class="card">
             <p class="title">"${book.title}"</p>
             <p class="author">${book.author}</p>
             <p class="pages">${book.pages} pages</p>
-            <button class="status-button not-read">${book.status}</button>
+            <button class="status-button ${statusCssClass}">${book.status}</button>
             <button class="remove-button">Remove</button>
         </div>
     `
+
 })
