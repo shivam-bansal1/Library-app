1c0b4e03d40ef38c7c6d245c26129a076ab3bd0f
Shivam Bansal <shivambnsal@gmail.com>
2024-08-24 00:23:39 +0530

    Functionality to add new book dynamically

--- script.js
+++ script.js
@@ -10,49 +10,64 @@ function Book(title, author, pages, status) {
     return {title : this.title,
             author : this.author,
             pages : this.pages,
-            status : this.status }
+            status : this.status 
+        }
 }
 
 function addToLibrary(book) {
     myLibrary.push(book);
 }
 
-// Dummy books
-let Book1 = new Book("Book1", "Author1", 345, "Not read yet");
-addToLibrary(Book1);
-let Book2 = new Book("Book2", "Author2", 35, "Read");
-addToLibrary(Book2);
-
-let cardContainer = document.querySelector('.card-container') ;
-
-myLibrary.forEach((book) => {
-    const statusCssClass = book.status == 'Read' ? "read" : "not-read";
-    cardContainer.innerHTML += `
-        <div class="card">
-            <p class="title">"${book.title}"</p>
-            <p class="author">${book.author}</p>
-            <p class="pages">${book.pages} pages</p>
-            <button class="status-button ${statusCssClass}">${book.status}</button>
-            <button class="remove-button">Remove</button>
-        </div>
-    `
-
-})
-
 // Show dialog
 const bodyElement = document.querySelector('body'); 
-const dialog = document.querySelector("dialog");
+const dialogBox = document.querySelector(".dialog-box");
+const dialogBoxDiv = document.querySelector(".dialog-div");
 const addBookButton = document.querySelector('.add-book-button');
+
 addBookButton.addEventListener('click', () => {
-    dialog.showModal();
+    dialogBox.showModal();
     bodyElement.style.opacity = 0.2;
 });
 
 // Close dialog
-const dialogBox = document.querySelector(".dialog-box");
-const dialogBoxDiv = document.querySelector(".dialog-div");
-dialogBox.addEventListener('click', () => { 
-    dialog.close();
+function closeDialogBox() {
+    dialogBox.close();
     bodyElement.style.opacity = 1;
-});
+}
+dialogBox.addEventListener('click', closeDialogBox);
 dialogBoxDiv.addEventListener('click', (event) => event.stopPropagation());
+
+// Submit form to add new book
+const dialogForm = document.querySelector('.dialog-form');
+dialogForm.addEventListener('submit', (event)=> {
+    // Prevents default behaviour i.e. doesn't try to submit and refresh the page
+    event.preventDefault();
+
+    const title = document.querySelector('.dialog-book-title').value;
+    const author = document.querySelector('.dialog-book-author').value;
+    const pages = document.querySelector('.dialog-book-pages').value;
+    const readingStatus = document.querySelector('.reading-status').checked;
+
+    let newBook = new Book(title, author, pages, readingStatus);
+    addToLibrary(newBook);
+    dialogForm.reset();
+
+    // Create book cards
+    const cardContainer = document.querySelector('.card-container') ;
+    cardContainer.innerHTML = '';
+
+    myLibrary.forEach((book) => {
+        const statusCssClass = book.status == true ? "read" : "not-read";
+        cardContainer.innerHTML += `
+            <div class="card">
+                <p class="title">"${book.title}"</p>
+                <p class="author">${book.author}</p>
+                <p class="pages">${book.pages} pages</p>
+                <button class="status-button ${statusCssClass}">${book.status==true ? "Read": "Not read yet"}</button>
+                <button class="remove-button">Remove</button>
+            </div>
+        `
+    })
+
+    closeDialogBox();
+});
