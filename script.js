a7ad33e3c37ded5b9633b4a6c17846df030c6e91
Shivam Bansal <shivambnsal@gmail.com>
2024-10-29 02:25:01 +0530

    Add local Storage

--- script.js
+++ script.js
@@ -2,63 +2,62 @@ import { validateField } from "./validate.js";
 
 // Book Class
 class Book {
-  constructor(title, author, pages, status) {
+  constructor(id, title, author, pages, status) {
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.status = status;
   }
+}
 
-  static myLibrary = [];
+class BookManager {
+  constructor() {
+    this.myLibrary = this.loadFromLocalStorage();
+  }
 
-  addToLibrary() {
-    Book.myLibrary.push({
-      title: this.title,
-      author: this.author,
-      pages: this.pages,
-      status: this.status,
+  loadFromLocalStorage() {
+    const myLibrary = localStorage.getItem("myLibrary");
+    return myLibrary ? JSON.parse(myLibrary) : [];
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem("myLibrary", JSON.stringify(this.myLibrary));
+  }
+
+  addToLibrary(title, author, pages, status) {
+    const idCount = Math.floor(Math.random() * 100000) + 10000;
+    console.log(idCount);
+    const newBook = new Book(idCount, title, author, pages, status);
+    console.log(newBook);
+    console.log(this.myLibrary);
+
+    if (this.myLibrary.length === 0) {
+      this.myLibrary.push(newBook);
+    }
+    this.myLibrary.some((book) =>
+      book.title === title && book.author === author
+        ? alert(`${title} by ${author} already saved!!!`)
+        : this.myLibrary.push(newBook)
+    );
+
+    console.log(this.myLibrary);
+    this.saveToLocalStorage();
+  }
+
+  removeFromLibrary(id) {
+    this.myLibrary = this.myLibrary.filter((book) => book.id != id);
+    this.saveToLocalStorage();
+  }
+
+  updateReadingStatus(id) {
+    this.myLibrary.forEach((book) => {
+      if (book.id == id) book.status = !book.status;
     });
+
+    this.saveToLocalStorage();
   }
 }
-
-function createBookCard() {
-  const cardContainer = document.querySelector(".card-container");
-  cardContainer.innerHTML = "";
-
-  Book.myLibrary.forEach((book) => {
-    const statusCssClass = book.status == true ? "read" : "not-read";
-    cardContainer.innerHTML += `
-          <div class="card">
-          <p class="title">"${book.title}"</p>
-          <p class="author">${book.author}</p>
-          <p class="pages">${book.pages} pages</p>
-          <button class="status-button ${statusCssClass}">
-                ${book.status == true ? "Read" : "Not read yet"}
-          </button>
-          <button class="remove-button">Remove</button>
-          </div>
-          `;
-  });
-}
-
-// Show dialog
-const bodyElement = document.querySelector("body");
-const dialogBox = document.querySelector(".dialog-box");
-const dialogBoxDiv = document.querySelector(".dialog-div");
-const addBookButton = document.querySelector(".add-book-button");
-
-addBookButton.addEventListener("click", () => {
-  dialogBox.showModal();
-  bodyElement.style.opacity = 0.2;
-});
-
-// Close dialog
-function closeDialogBox() {
-  dialogBox.close();
-  bodyElement.style.opacity = 1;
-}
-// dialogBox.addEventListener("click", closeDialogBox);
-// dialogBoxDiv.addEventListener("click", (event) => event.stopPropagation());
 
 function addNewBook() {
   // Submit form to add new book
@@ -93,13 +92,12 @@ function addNewBook() {
       error.textContent = "";
       error.className = "error";
 
-      let newBook = new Book(
+      BookManagerObject.addToLibrary(
         title.value,
         author.value,
         pages.value,
         readingStatus
       );
-      newBook.addToLibrary();
 
       // Create book cards
       createBookCard();
@@ -115,27 +113,75 @@ function removeBook() {
   bookCardsContainer.addEventListener("click", function (event) {
     // Remove book
     if (event.target.classList.contains("remove-button")) {
-      const cardDiv = event.target.closest(".card");
-      cardDiv.remove();
+      const bookId = event.target.dataset.bookId;
+      BookManagerObject.removeFromLibrary(bookId);
+      window.location.reload();
     }
-    // Change Reading Status
-    updateReadStatus(event);
   });
 }
 
-function updateReadStatus(event) {
+function updateReadStatus() {
   // Change Reading Status
-  if (event.target.classList.contains("status-button")) {
-    const readingStatusButton = event.target.closest(".status-button");
-    if (event.target.classList.contains("not-read")) {
-      readingStatusButton.classList = "status-button read";
-      readingStatusButton.textContent = "Read";
-    } else if (event.target.classList.contains("read")) {
-      readingStatusButton.classList = "status-button not-read";
-      readingStatusButton.textContent = "Not read yet";
+  const bookCardsContainer = document.querySelector(".card-container");
+  bookCardsContainer.addEventListener("click", function (event) {
+    if (event.target.classList.contains("status-button")) {
+      console.log(event.target.dataset);
+      BookManagerObject.updateReadingStatus(event.target.dataset.id);
+      window.location.reload();
     }
-  }
+  });
 }
 
-addNewBook();
-removeBook();
+function createBookCard() {
+  const cardContainer = document.querySelector(".card-container");
+  cardContainer.innerHTML = "";
+  const library = BookManagerObject.loadFromLocalStorage("myLibrary");
+
+  library.forEach((book) => {
+    const statusCssClass = book.status == true ? "read" : "not-read";
+    cardContainer.innerHTML += `
+          <div class="card">
+          <p class="title">"${book.title}"</p>
+          <p class="author">${book.author}</p>
+          <p class="pages">${book.pages} pages</p>
+          <button class="status-button ${statusCssClass}" data-id="${book.id}">
+              ${book.status == true ? "Read" : "Not read yet"}
+          </button>
+          <button class="remove-button"
+            data-book-id="${book.id}">
+            Remove
+          </button>
+          </div>
+          `;
+  });
+}
+
+// Show dialog
+const bodyElement = document.querySelector("body");
+const dialogBox = document.querySelector(".dialog-box");
+const dialogBoxDiv = document.querySelector(".dialog-div");
+const addBookButton = document.querySelector(".add-book-button");
+
+// Close dialog
+function closeDialogBox() {
+  dialogBox.close();
+  bodyElement.style.opacity = 1;
+}
+
+function main() {
+  addBookButton.addEventListener("click", () => {
+    dialogBox.showModal();
+    bodyElement.style.opacity = 0.2;
+  });
+
+  dialogBox.addEventListener("click", closeDialogBox);
+  dialogBoxDiv.addEventListener("click", (event) => event.stopPropagation());
+
+  addNewBook();
+  removeBook();
+  createBookCard();
+  updateReadStatus();
+}
+
+const BookManagerObject = new BookManager();
+main();
